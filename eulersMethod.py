53537f83a51f0692be24a03ea860c2bc40bc7d5a
elisebsm <elisesmebye@gmail.com>
2019-03-21 11:19:50 +0100

    kalkulering

--- eulersMethod.py
+++ eulersMethod.py
@@ -3,6 +3,11 @@ import scipy.interpolate as interp
 import matplotlib.pyplot as plt
 plt.style.use('bmh')    # Nicer looking plots
 
+
+####### variabler
+
+m = 1
+
 ####################
 # fra numfy
 ####################
@@ -16,32 +21,82 @@ c1 = 2/3            # kuleskall
 c2 = 2/5            # kule
 c3 = 1              # tynn sirkulær skive
 
-I0 = c*m*r**2            # kg m^2 (moment of inertia)
+I01 = c1*m*r**2            # kg m^2 (moment of inertia)
+I02 = c2*m*r**2
+I03 = c3*m*r**2
 
-########################333
+########################
+
+
+N = len(yi)               #   (# of mounts)
+xi = np.linspace(0, L, N) # m (x-positions)
+plt.plot(xi, yi)
+plt.show()
+
+
+get_y = interp.CubicSpline(xi, yi, bc_type="natural")
+
+# KALKULERER theta
+# trvalues - track values
+#
+# SYNTAX
+# [y,dydx,d2ydx2,alpha,R]=trvalues(p,x)
+#
+# INPUT
+# p: the n+1 coefficients of a polynomial of degree n, given in descending
+# order. (For instance the output from p=iptrack(filename).)
+# x: ordinate value at which the polynomial is evaluated.
+#
+# OUTPUT
+# [y,dydx,d2ydx2,alpha,R]=trvalues(p,x) returns the value y of the
+# polynomial at x, the derivative dydx and the second derivative d2ydx2 in
+# that point, as well as the slope alpha(x) and the radius of the
+# osculating circle.
+# The slope angle alpha is positive for a curve with a negative derivative.
+# The sign of the radius of the osculating circle is the same as that of
+# the second derivative.
+
+import numpy as np
+
+
+def trvalues(p,x):
+	y=np.polyval(p,x)
+	dp=np.polyder(p)
+	dydx=np.polyval(dp,x)
+	ddp=np.polyder(dp)
+	d2ydx2=np.polyval(ddp,x)
+	alpha=np.arctan(-dydx)
+	R=(1.0+dydx**2)**1.5/d2ydx2
+	return [y,dydx,d2ydx2,alpha,R]
+
+
+
+#, innhenter x og y verdier og plotter
+# iptrack - interpolate track
+#
+# SYNTAX
+# p=iptrack(filename)
+#
+# INPUT
+# filename: data file containing exported tracking data on the standard
+# Tracker export format
+#
+# mass_A
+# t	x	y
+# 0.0	-1.0686477620876644	42.80071293284619
+# 0.04	-0.714777136706708	42.62727536827738
+# ...
+#
+# OUTPUT
+# p=iptrack(filename) returns the coefficients of a polynomial of degree 15
+# that is the least square fit to the data y(x). Coefficients are given in
+# descending powers.
+
+import numpy as np
+
 def iptrack(filename):
 	data=np.loadtxt(filename,skiprows=2)
-	return np.polyfit(data[:,1],data[:,2],15), data[0,1]
-
-p, x_0 = iptrack(track)
-v_0 = 0
-def find_x(x_0):
-    x_points = []
-    v_points = []
-    x_points[0]=x_0
-    v_points[0] = 0
-
-    #h = steglengden
-    h =
-    sluttid-starttid/antall x-punkter
-    for i in range (1,N+1):
-        x_points[i] = x_points[i-1] + h
-
-    return
-
-
-
-
+	return np.polyfit(data[:,1],data[:,2],15)
 
 
 
