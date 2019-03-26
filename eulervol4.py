ca5db68e4a8b7ae68d6caddd613d5418de72fd9a
elisebsm <elisesmebye@gmail.com>
2019-03-26 13:54:33 +0100

    Endelig euler og plotting

--- eulervol4.py
+++ eulervol4.py
@@ -38,59 +38,54 @@ def trvalues(p, x_old):
     return [y, dydx, d2ydx2, alpha, R]
 
 
+
 t_0 = 0
 v_0 = 1.135
 t = np.zeros(N + 1)
 v = np.zeros(N + 1)
-def euler_vt():
-    t[0] = t_0
-    v[0] = v_0
-    t_old = t_0
-    v_old = v_0
-    for n in range(N):
-        val = trvalues(p, v_old)
-        v_new = v_old + h * (g*np.sin(val[3])/(1+c))
-        t[n + 1] = t_old + h
-        v[n + 1] = v_new
+t[0] = t_0
+v[0] = v_0
+t_old = t_0
+v_old = v_0
+for n in range(N):
+    val = trvalues(p, x_old)
+    v_new = v_old + h * (g*np.sin(val[3])/(1+c2))
+    t[n + 1] = t_old + h
+    v[n + 1] = v_new
 
-        t_old = t_old + h
-        v_old = v_new
-    return [t, v]
+    t_old = t_old + h
+    v_old = v_new
+#print(r'x_N = %f' % v_old)
 
 
 t2_0 = 0
 x_0 = -0.427
 t2 = np.zeros(N + 1)
 x = np.zeros(N + 1)
-def euler_xt():
-    t2[0] = t2_0
-    x[0] = x_0
-    t2_old = t2_0
-    x_old = x_0
+t2[0] = t2_0
+x[0] = x_0
+t2_old = t2_0
+x_old = x_0
 
-    for n in range(N):
-        val = trvalues(p, x_old)
-        x_new = x_old + h * (v[n])
-        t2[n + 1] = t2_old + h
-        x[n + 1] = x_new
-        t2_old = t2_old + h
-        x_old = x_new
-    return [t2, x]
+for n in range(N):
+    val = trvalues(p, x_old)
+    x_new = x_old + h * (v[n])
+    t2[n + 1] = t2_old + h
+    x[n + 1] = x_new
+    t2_old = t2_old + h
+    x_old = x_new
 
 
 def x_av_t(filename):   # gir eksperementielle verdier for x v t
-    tpos = []
-    vpos = []
     xpos = []
     ypos = []
-
+    tpos = []
     data = np.loadtxt(filename, skiprows=2)
     for line in data:
         tpos.append(line[0])
-        vpos.append(line[1])
-        xpos.append(line[2])
-        ypos.append(line[3])
-    return [tpos, vpos, xpos, ypos]
+        xpos.append(line[1])
+        ypos.append(line[2])
+    return [xpos, ypos, tpos]
 
 
 punkterLB = x_av_t(r'C:\Users\Elise\Documents\6. semester\Fysikk\LB.txt')
@@ -105,19 +100,19 @@ def plot_xt():
     plt.ylabel(r'$x(t)$')
     plt.xlabel(r'$t$')
     plt.grid()
-    plt.plot(t2, x, color = '#4daf4a', label=labelNum)
-    plt.plot(p0, p2, color = '#377eb8', label=labelEks)
-    plt.legend()
+    plt.plot(t2, x, color = '#4daf4a')
+    plt.plot(punkter[2], punkter[0], color = '#377eb8')
     plt.show()
 
+
 def plot_vt():
+    #euler_vt()
     plt.figure()
     plt.ylabel(r'$v(t)$')
     plt.xlabel(r'$t$')
     plt.grid()
-    plt.plot(t, v, color='#4daf4a', label=labelNum)
-    plt.plot(p0, p1, color='#377eb8', label=labelEks)
-    plt.legend()
+    plt.plot(t, v, color='#4daf4a')
+    plt.plot(punkter[2], vt, color='#377eb8')
     plt.show()
 
 
@@ -125,24 +120,14 @@ cPP = 2 / 3
 cLB = 2 / 5
 cR = 1
 cAll = [cLB, cPP, cR]
-labelAllEks = ["Liten Ball Eksperementiell", "Ping pong Eksperementiell", "Ring Eksperementiell"]
-labelAllNum = ["Liten Ball Teoretisk", "Ping pong Teoretisk", "Ring Teoretisk"]
+plot_xt()
+
+
 for n in range (3):
-    labelEks = labelAllEks[n]
-    labelNum = labelAllNum[n]
-    p0 = punkterAll[n][0]
-    p1 = punkterAll[n][1]
-    p2 = punkterAll[n][2]
-    p3 = punkterAll[n][3]
+    punkter = punkterAll[n]
     c = cAll[n]
     p = pAll[n]
-    euler_vt()
     plot_vt()
-    euler_xt()
-    #plot_xt()
-
-
-
 
 
 
